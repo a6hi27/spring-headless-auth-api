ba13499cf7a47523da505c0a4fc48dbac7955277
Abhi <abhinavm2705@gmail.com>
2026-03-08 19:52:38 +0530

    Initial commit: Spring Boot skeleton, dependencies, DDD structure and application.yml

---
+++ src/main/java/com/bolt/headless_auth_api/HeadlessAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.bolt.headless_auth_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadlessAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadlessAuthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bolt/headless_auth_api/HeadlessAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolt.headless_auth_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadlessAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
