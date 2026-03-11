b1c400db8cb2e216e463d359647ed7e3c97bbc57
Abhi <abhinavm2705@gmail.com>
2026-03-11 22:53:17 +0530

    feat: implement AuthController REST endpoints for OTP generation and validation

---
+++ src/main/java/com/bolt/headless_auth_api/auth/AuthController.java
@@ -0,0 +1,45 @@
+package com.bolt.headless_auth_api.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import jakarta.validation.Valid;
+
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final OtpService otpService;
+
+    @PostMapping("/generate-otp")
+    public ResponseEntity<String> generateOtp(
+            @Valid @RequestBody GenerateOtpRequest request) {
+
+        // Call the OtpService to generate and send the OTP using the email from the request
+        String email = request.email();
+        otpService.generateAndSendOtp(email);
+
+        // Return a 200 OK success message
+        return ResponseEntity.ok("OTP generated and sent successfully to " + request.email());
+    }
+
+    @PostMapping("/validate-otp")
+    public ResponseEntity<String> validateOtp(
+            @Valid @RequestBody ValidateOtpRequest request) {
+
+        // Call the OtpService to validate the OTP.
+        String email = request.email();
+        String otp = request.otp();
+        boolean isValid = otpService.validateOtp(email, otp);
+
+        // If it returns true, return a 200 OK ("OTP is valid").
+        if (isValid)
+            return ResponseEntity.ok("OTP is valid");
+
+        // If it returns false, return a 401 Unauthorized ("Invalid or expired OTP").
+        return new ResponseEntity<>("Invalid or expired OTP", HttpStatus.UNAUTHORIZED);
+    }
+}
