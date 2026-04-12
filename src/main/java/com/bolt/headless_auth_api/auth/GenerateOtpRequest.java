cde5e3e58fcc7288336a186fdc4a76a210dc7522
Abhi <abhinavm2705@gmail.com>
2026-04-12 21:22:38 +0530

    fix(auth): enforce strict regex validation for email requests

--- src/main/java/com/bolt/headless_auth_api/auth/GenerateOtpRequest.java
+++ src/main/java/com/bolt/headless_auth_api/auth/GenerateOtpRequest.java
@@ -4,8 +4,9 @@ package com.bolt.headless_auth_api.auth;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record GenerateOtpRequest(@Email(message = "Please provide a valid email!") @NotBlank(message = "Email " +
-        "address cannot be blank!") String email) {
+public record GenerateOtpRequest(
+        @Email(message = "Please provide a valid email address with a proper domain (e.g., user@domain.com)", regexp = "^[\\w.%+-]+@[A-Za-z\\d.-]+\\.[A-Za-z]{2,6}$") @NotBlank(message = "Email " +
+                "address cannot be blank!") String email) {
 
     public GenerateOtpRequest {
         if (email() != null)
